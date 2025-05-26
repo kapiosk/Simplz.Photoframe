1a32e7d765ca491519b44c2f3c94ccd5e03ee434
AZ <speedsterk@hotmail.com>
2025-05-26 17:50:01 +0300

    Refactor GPIO/button handling: enable GPIO input and output configuration for button and LED functionality in production

--- app.py
+++ app.py
@@ -104,10 +104,13 @@ if not app.debug:
     SW_B = 6
     SW_C = 25
     SW_D = 24
+    LED_PIN = 13
     BUTTONS = [SW_A, SW_B, SW_C, SW_D]
     LABELS = ["A", "B", "C", "D"]
-# INPUT = gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
-# chip = gpiodevice.find_chip_by_platform()
+    INPUT = gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
+    chip = gpiodevice.find_chip_by_platform()
+    led = chip.line_offset_from_id(LED_PIN)
+    gpio = chip.request_lines(consumer="inky", config={led: gpiod.LineSettings(direction=Direction.OUTPUT, bias=Bias.DISABLED)})
 # OFFSETS = [chip.line_offset_from_id(id) for id in BUTTONS]
 # line_config = dict.fromkeys(OFFSETS, INPUT)
 # request = chip.request_lines(consumer="photoframe-buttons", config=line_config)
